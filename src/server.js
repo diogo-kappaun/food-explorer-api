de93764e54f252cbba5780591a19d3172e7e2d00
diogo-kappaun <diogohkappaun@gmail.com>
2024-05-07 22:32:44 -0300

    feat: Create AppError

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
 import express from 'express'
+import 'express-async-errors'
+
+import { AppError } from './utils/AppError'
 
 import 'dotenv/config'
 
 const app = express()
 const PORT = process.env.PORT || 3333
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+
+  console.log(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(PORT, () => console.log(`Server is running | Port: ${PORT}`))
