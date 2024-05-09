87601071d544ea730fc12c6c35580d848c03cf53
diogo-kappaun <diogohkappaun@gmail.com>
2024-05-08 22:50:13 -0300

    feat: Create connection to the knex

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+import path from 'path'
+import Path from './src/utils/Path.js'
+
+export default {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(
+        Path.dirname(import.meta.url),
+        'src',
+        'database',
+        'database.db',
+      ),
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(
+        Path.dirname(import.meta.url),
+        'src',
+        'database',
+        'knex',
+        'migration',
+      ),
+    },
+  },
+}
