2919a8a84f7ab1db027e97a6e2d449d29eb3e6d4
diogo-kappaun <diogohkappaun@gmail.com>
2024-05-11 20:41:08 -0300

    feat: Create order-items migration

---
+++ src/database/knex/migration/20240511233621_order_items.js
@@ -0,0 +1,16 @@
+export const up = (knex) =>
+  knex.schema.createTable('order-items', (table) => {
+    table.increments('id')
+    table.integer('dishe_id').references('id').inTable('dishes')
+    table
+      .integer('order_id')
+      .references('id')
+      .inTable('orders')
+      .onDelete('CASCADE')
+    table.integer('amount').notNullable()
+    table.float('price').notNullable()
+    table.timestamp('created_at').default(knex.fn.now())
+    table.timestamp('updated_at').default(knex.fn.now())
+  })
+
+export const down = (knex) => knex.schema.dropTable('order-items')
