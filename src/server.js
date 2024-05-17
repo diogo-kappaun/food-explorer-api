dc1721b7f54d78f86c4194c56113730f5efac6ae
diogo-kappaun <diogohkappaun@gmail.com>
2024-05-16 22:23:52 -0300

    feat: Create routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
-import database from './database/sqlite/index.js'
+import { routes } from './routes/index.js'
 import { AppError } from './utils/AppError.js'
 
-database()
-
 const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
