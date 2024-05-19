896279de92f0015b93762bf2d04562e4c0e7e3f8
diogo-kappaun <diogohkappaun@gmail.com>
2024-05-19 14:58:55 -0300

    feat: Create user update functionality

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,8 @@
-import pkg from 'bcryptjs'
+import bcryptjs from 'bcryptjs'
 import { dbConnection } from '../database/knex/index.js'
 import { AppError } from '../utils/AppError.js'
 
-const { hash } = pkg
+const { hash, compare } = bcryptjs
 
 const knex = dbConnection
 
@@ -32,5 +32,57 @@ export class UserController {
     })
 
     return response.json(`Usuário cadastrado com sucesso!`)
+  }
+
+  async update(request, response) {
+    const { name, email, currentPassword, newPassword } = request.body
+    const userId = request.user.id
+
+    const user = await knex('users').where({ id: userId }).first()
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado!')
+    }
+
+    if (email) {
+      const emailAlreadyInUse = await knex('users').where({ email }).first()
+
+      if (emailAlreadyInUse && emailAlreadyInUse.id !== userId) {
+        throw new AppError('E-mail informado está em uso!')
+      }
+    }
+
+    user.name = name || user.name
+    user.email = email || user.email
+
+    if (!currentPassword && newPassword) {
+      throw new AppError('Senha atual não informada!')
+    }
+
+    if (currentPassword && newPassword) {
+      const checksCurrentPasswordIsValid = await compare(
+        currentPassword,
+        user.password,
+      )
+
+      if (!checksCurrentPasswordIsValid) {
+        throw new AppError('Senha atual informada não é valida!')
+      }
+
+      const hashedNewPassword = await hash(newPassword, 8)
+
+      user.password = hashedNewPassword
+    }
+
+    await knex('users')
+      .update({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        updated_at: knex.fn.now(),
+      })
+      .where({ id: userId })
+
+    return response.json('Usuário atualizado com sucesso!')
   }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/usersController.js'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js'
 
 const usersRoutes = Router()
 
 const usersController = new UserController()
 
 usersRoutes.post('/', usersController.create)
+usersRoutes.put('/', ensureAuthenticated, usersController.update)
 
 export { usersRoutes }
