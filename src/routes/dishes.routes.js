676ebe1918522412da7911fbd668577d9eef9f8d
diogo-kappaun <diogohkappaun@gmail.com>
2024-05-21 19:57:19 -0300

    feat: Add ensure authenticated to route

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { DishesController } from '../controllers/dishesController.js'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js'
 
 const dishesController = new DishesController()
 
 const dishesRoutes = Router()
 
-dishesRoutes.post('/', dishesController.create)
+dishesRoutes.post('/', ensureAuthenticated, dishesController.create)
 
 export { dishesRoutes }
