a83d2ad322935bb29d742d5a17f4d8cecd17ee27
diogo-kappaun <diogohkappaun@gmail.com>
2024-05-21 19:58:09 -0300

    feat: Add logic to add dishes

--- src/controllers/dishesController.js
+++ src/controllers/dishesController.js
@@ -1,5 +1,27 @@
+import { dbConnection } from '../database/knex/index.js'
+
+const knex = dbConnection
+
 export class DishesController {
   async create(request, response) {
-    return response.json('Hello, world!')
+    const { name, description, price, category, ingredients } = request.body
+
+    const [disheId] = await knex('dishes').insert({
+      name,
+      description,
+      price_in_cents: price,
+      category,
+    })
+
+    const ingredientsInsert = ingredients.map((name) => {
+      return {
+        dishe_id: disheId,
+        name,
+      }
+    })
+
+    await knex('ingredients').insert(ingredientsInsert)
+
+    return response.json('Prato cadastrado com sucesso!')
   }
 }
