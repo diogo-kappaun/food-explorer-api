ac9c0f9052cee401573645d980fcc7d35d3f267b
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-04 14:04:16 -0300

    feat: Add the route for the files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
+import uploadConfig from './config/upload.js'
 import { routes } from './routes/index.js'
 import { AppError } from './utils/AppError.js'
 
@@ -8,6 +9,8 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
