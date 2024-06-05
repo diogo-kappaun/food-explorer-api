5ba76ea72805cb9b5bea3365956d2979966cf2b2
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-05 14:21:07 -0300

    feat: Add upload to Cloudinary

--- src/controllers/dishImageController.js
+++ src/controllers/dishImageController.js
@@ -1,16 +1,14 @@
-import { dbConnection } from '../database/knex/index.js'
-import { DiskStorage } from '../providers/DiskStorage.js'
+import path from 'path'
+import cloudinary from '../config/cloudinary.js'
+import { dbConnection as knex } from '../database/knex/index.js'
+import diskStorage from '../providers/DiskStorage.js'
 import { AppError } from '../utils/AppError.js'
-
-const knex = dbConnection
-
-const diskStorage = new DiskStorage()
 
 export class DishImageController {
   async update(request, response) {
     const dishId = request.query.id
 
-    const dishFilename = request.file.filename
+    const imageFilename = request.file.filename
 
     const dish = await knex('dishes').where({ id: dishId }).first()
 
@@ -18,16 +16,28 @@ export class DishImageController {
       throw new AppError('Prato não encontrado!')
     }
 
-    if (dish.image) {
-      await diskStorage.deleteFile(dish.image)
+    if (dish.image_id) {
+      try {
+        await cloudinary.uploader.destroy(dish.image_id)
+      } catch (error) {
+        throw new AppError('Não foi possível apagar a imagem!')
+      }
     }
 
-    const filename = await diskStorage.saveFile(dishFilename)
+    try {
+      const image = path.resolve('tmp', imageFilename)
 
-    dish.image = filename
+      const { public_id } = await cloudinary.uploader.upload(image)
 
-    await knex('dishes').update(dish).where({ id: dishId })
+      await diskStorage.deleteFile(image)
 
-    return response.json({ dish })
+      dish.image_id = public_id
+
+      await knex('dishes').update(dish).where({ id: dishId })
+    } catch (error) {
+      throw new AppError(error)
+    }
+
+    return response.json('Imagem atualizada com sucesso!')
   }
 }
