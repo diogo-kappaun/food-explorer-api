3ebc93e2a1238ca4f464963b4e3ab70a692650cb
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-05 15:50:31 -0300

    refactor: Delete category column

--- src/database/knex/migration/20240511213739_dishes.js
+++ src/database/knex/migration/20240511213739_dishes.js
@@ -4,7 +4,6 @@ export const up = (knex) =>
     table.text('name').notNullable()
     table.text('description')
     table.integer('price_in_cents').notNullable()
-    table.text('category').notNullable()
     table.text('image_id').nullable()
     table.timestamp('created_at').default(knex.fn.now())
     table.timestamp('updated_at').default(knex.fn.now())
