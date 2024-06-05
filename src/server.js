635f8ae3f576658915d56b36333fcfbbf94e7a6e
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-05 10:53:23 -0300

    chore(refactor): Remove unused static file route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
-import uploadConfig from './config/upload.js'
 import { routes } from './routes/index.js'
 import { AppError } from './utils/AppError.js'
 
@@ -9,8 +8,6 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
-
-app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
