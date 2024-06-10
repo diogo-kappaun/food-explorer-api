ccfd79822e79bc8513b5d910175874b4379ca39a
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-10 15:34:28 -0300

    fix: Image was not deleted when dish was deleted

--- src/services/DishDeleteService.js
+++ src/services/DishDeleteService.js
@@ -1,3 +1,4 @@
+import cloudinary from '../config/cloudinary.js'
 import { AppError } from '../utils/AppError.js'
 
 export class DishDeleteService {
@@ -8,6 +9,16 @@ export class DishDeleteService {
   async execute(id) {
     if (id === '') {
       throw new AppError('Prato não informado!')
+    }
+
+    const dish = await this.dishRepository.getDishByID(id)
+
+    if (dish.image_id) {
+      try {
+        await cloudinary.uploader.destroy(dish.image_id)
+      } catch (error) {
+        throw new AppError('Não foi possível apagar a imagem!')
+      }
     }
 
     await this.dishRepository.delete(id)
