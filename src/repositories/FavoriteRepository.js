3b328e10358e64a57725e6fa228d80406c9bb54d
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-10 10:06:51 -0300

    refactor: Create favorite repository

---
+++ src/repositories/FavoriteRepository.js
@@ -0,0 +1,33 @@
+import { dbConnection as knex } from '../database/knex/index.js'
+import { AppError } from '../utils/AppError.js'
+
+export class FavoriteRepository {
+  async findByUserAndDishID({ user_id, dish_id }) {
+    const isFavorite = await knex('favorites')
+      .where({ user_id, dish_id })
+      .first()
+
+    return isFavorite
+  }
+
+  async create({ user_id, dish_id }) {
+    await knex('favorites')
+      .insert({ user_id, dish_id })
+      .catch((err) => {
+        throw new AppError(err)
+      })
+  }
+
+  async getUserFavorites({ user_id }) {
+    const favorites = await knex('favorites')
+      .select('dishes.id', 'dishes.image_id', 'dishes.name')
+      .where({ user_id })
+      .innerJoin('dishes', 'dishes.id', 'favorites.dish_id')
+
+    return favorites
+  }
+
+  async delete({ user_id, dish_id }) {
+    await knex('favorites').where({ user_id, dish_id }).delete()
+  }
+}
