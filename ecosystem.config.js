9e63c39628911db37015133b212a66089b6c48c5
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-07 19:46:29 -0300

    feat: Add pm2 for deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+export default {
+  apps: [
+    {
+      name: 'app',
+      script: './src/server.js',
+      instances: 'max',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
